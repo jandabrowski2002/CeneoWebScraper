59eb9da1d9b855eec7537adcb5c2885fbdd1cfa0
jandabrowski2002 <s222944@student.uek.krakow.pl>
2022-06-14 12:55:53 +0200

    2022-06-14 JD

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,31 @@ from flask import render_template
 
 @app.route("/")
 @app.route("/index")
+
 def index():
-    return render_template("index.html.jinja",name=name)
+    name = 'Jan Dąbrowski'
+    return render_template('index.html.jinja', name=name)
+
+
+@app.route('/author')
+@app.route('/templates/author')
+def author():
+    index_number = '222944'
+    field_of_study = 'Applied Informatics'
+    return render_template('author.html.jinja', index_number=index_number, field_of_study=field_of_study)
+
+@app.route("/extract")
+def extract():
+    return render_template("extract.html.jinja")
+
+@app.route("/products")
+def products():
+    return render_template("products.html.jinja")
+
+@app.route("/product/<product_id>")
+def product():
+    return render_template("product.html.jinja")
+
+@app.route("/opinions")
+def opinions():
+    return render_template("opinions.html.jinja")
